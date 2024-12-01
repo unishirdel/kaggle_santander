3612bf30c6f1ad27f1cada898fee8345f061f238
Mammad Shirdel <mammadshirdel@gmail.com>
2024-12-01 11:50:56 +0100

    id related columns are dropped

--- src/data_preprocessing/data_obj.py
+++ src/data_preprocessing/data_obj.py
@@ -9,18 +9,24 @@ class Data:
         df: pd.DataFrame = None,
         name: str = "train.csv",
         target_col_name: str = None,
+        id_columns: list[str] = None,
     ):
         if df is None:
             self._df = self.load_data(name)
         else:
             self._df = df
 
+        if id_columns is None:
+            id_columns = [col for col in self._df.columns if "id" in col.lower()]
+
         if target_col_name is not None:
             self._target = self._df[target_col_name]
             self._features = self._df.drop(target_col_name, axis=1)
+            self._features = self._df.drop(id_columns, axis=1)
         else:
             self._target = None
             self._features = self._df.copy()
+            self._features = self._df.drop(id_columns, axis=1)
 
     @property
     def features(self):
